f0857e6081439e4fb17a9e7be66ef2fb42dd27b8
Babybew <waroon1515@gmail.com>
2021-03-20 11:43:45 +0700

    create checkfibo and checkprime

--- isprimeorfibo/src/app/app.component.ts
+++ isprimeorfibo/src/app/app.component.ts
@@ -3,8 +3,52 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'isprimeorfibo';
+  number: number = 0;
+  result: boolean = false;
+  type: any;
+
+  checkValue() {
+    if (this.type === 'isPrime') {
+      this.result = this.checkisPrime();
+    } else if (this.type === 'isFibonacci') {
+      this.result = this.checkisFibonacci();
+    }
+  }
+
+  checkisPrime(): boolean {
+    if (this.number === 2) {
+      return true;
+    } else if (this.number > 1) {
+      for (var i = 2; i < this.number; i++) {
+        if (this.number % i !== 0) {
+          return true;
+        } else if (this.number === i * i) {
+          return false;
+        } else {
+          return false;
+        }
+      }
+    } else {
+      return false;
+    }
+    return false;
+  }
+
+  checkisFibonacci(): boolean {
+    if (
+      this.isSquare(5 * (this.number * this.number) - 4) ||
+      this.isSquare(5 * (this.number * this.number) + 4)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isSquare(n: number) {
+    return n > 0 && Math.sqrt(n) % 1 === 0;
+  }
 }
